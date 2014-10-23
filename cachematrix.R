c6212e490b0f7a0333ad6ee2e3c30d762e841c99
Oliver <Oliver.Binns@tatasteel.com>
2014-10-23 11:16:31 +0200

    Added makeCacheMatrix function with get/set/getinverse/setinverse functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,28 @@
 # Submission for Coursera Course rprog-008 Week 2 Programming Assignment
 # Oliver Binns
 
-# This file contains two functions...
+# This file contains two functions which can calculate and cache the inverse of a matrix
 
-# Function description
+
+# Caching function that can hold a matrix and its inverse through get/set/getinverse/setinvserse methods)
+# Called by the user the set a matrix and the cacheSolve function (below) for other methods
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solve) m <<- solve
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
-# Function description
+
+# Function that solves (find the inverse) of a matrix
+# Calls the makeCacheMatrix function (above)
 cacheSolve <- function(x, ...) {
-        # Return a matrix that is the inverse of 'x'
+
 }
